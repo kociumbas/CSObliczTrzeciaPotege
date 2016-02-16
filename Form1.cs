4d6ebd91ff0a924561f1d29a66f73b569f3078e4
kociumbas <kociumbas@outlook.com>
2016-02-16 01:04:27 +0100

    CSObliczTrzeciaPotege

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem1
+{
+    public partial class Form1 : Form
+    {
+        int x2;
+        int x3;
+        int wynik;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+          
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            x2 = x * x;
+            x3 = x2 * x;
+            wynik = x3;
+            textBox2.Text = wynik.ToString();
+        }
+    }
+}
